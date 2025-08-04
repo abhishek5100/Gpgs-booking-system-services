25893c5910874da3623e6dc8cb3cac6937aafa1f
abhishek gautam <iamdeveloperabhishek@gmail.com>
2025-08-04 22:38:18 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+// api/index.js
 require('dotenv').config();
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
 
-// Route imports (make sure these exist and use Express.Router())
 const sheetRoutes = require('./routes/sheetRoutes');
 const propertiesSheetRoutes = require('./routes/propertiesSheetRoutes');
 const propertySheetRoutes = require('./routes/propertySheetRoutes');
@@ -13,10 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Mount routes
-app.use('/', sheetRoutes);
-app.use('/', propertiesSheetRoutes);
-app.use('/', propertySheetRoutes);
+app.use('/api', sheetRoutes);
+app.use('/api', propertiesSheetRoutes);
+app.use('/api', propertySheetRoutes);
 
-// Export as serverless handler
+module.exports = app;
 module.exports.handler = serverless(app);
