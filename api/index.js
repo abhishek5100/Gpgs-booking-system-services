bd1bbf1a92b0e13ad02c1ce63528e3c3d0183601
abhishek gautam <iamdeveloperabhishek@gmail.com>
2025-08-09 20:37:00 +0530

    chnages

--- api/index.js
+++ api/index.js
@@ -23,3 +23,31 @@ app.use('/api', employeesRoutes);
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+
+
+// require('dotenv').config();
+// const express = require('express');
+// const cors = require('cors');
+
+// const sheetRoutes = require('./routes/sheetRoutes');
+// const propertiesSheetRoutes = require('./routes/propertiesSheetRoutes');
+// const propertySheetRoutes = require('./routes/propertySheetRoutes');
+// const employeesRoutes = require('./routes/employeesRoutes');
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Register routes
+// app.use('/', sheetRoutes);
+// app.use('/', propertiesSheetRoutes);
+// app.use('/', propertySheetRoutes);
+// app.use('/', employeesRoutes);
+
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
